71cc70c2ccfa3f7793fe100df58a3c05bd3d49ae
Arnau Mora <arnyminerz@proton.me>
2023-10-08 17:18:35 +0200

    Moved context provider
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- androidApp/src/main/java/com/arnyminerz/filamagenta/android/App.kt
+++ androidApp/src/main/java/com/arnyminerz/filamagenta/android/App.kt
@@ -5,7 +5,6 @@ import com.arnyminerz.filamagenta.account.AccountsProvider
 import com.arnyminerz.filamagenta.device.PlatformInformation
 import com.arnyminerz.filamagenta.storage.SettingsFactoryProvider
 import com.arnyminerz.filamagenta.storage.settingsFactory
-import com.arnyminerz.filamagenta.ui.browser.CustomBrowserUi
 
 class App: Application() {
     override fun onCreate() {
@@ -14,7 +13,6 @@ class App: Application() {
         settingsFactory = SettingsFactoryProvider(this).factory
 
         AccountsProvider(this).provide()
-        CustomBrowserUi.provideApplicationContext(this)
         PlatformInformation.checkIfCustomTabsSupported(this)
     }
 }
--- androidApp/src/main/java/com/arnyminerz/filamagenta/android/MainActivity.kt
+++ androidApp/src/main/java/com/arnyminerz/filamagenta/android/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.arnyminerz.filamagenta.ui.browser.CustomBrowserUi
 import com.arnyminerz.filamagenta.ui.screen.MainScreen
 import com.arnyminerz.filamagenta.ui.state.MainViewModel
 import com.arnyminerz.filamagenta.ui.theme.AppTheme
@@ -13,6 +14,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        CustomBrowserUi.provideApplicationContext(this)
 
         setContent {
             AppTheme {
