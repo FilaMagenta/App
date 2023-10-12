de1748c9d8e7015378f395216f6316a8fd10bd3d
Arnau Mora <arnyminerz@proton.me>
2023-10-12 16:07:00 +0200

    Made `accounts` lateinit
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- shared/src/commonMain/kotlin/com/arnyminerz/filamagenta/account/AccountsProvider.kt
+++ shared/src/commonMain/kotlin/com/arnyminerz/filamagenta/account/AccountsProvider.kt
@@ -3,7 +3,7 @@ package com.arnyminerz.filamagenta.account
 /**
  * Should be initialized by [AccountsProvider]. During runtime can be supposed to not being null.
  */
-var accounts: Accounts? = null
+lateinit var accounts: Accounts
 
 expect class AccountsProvider {
     fun provide(): Accounts
