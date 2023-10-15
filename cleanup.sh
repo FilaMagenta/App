72130819e9b65cc433208dff6d3de1cd1c469e27
Arnau Mora <arnyminerz@proton.me>
2023-10-15 10:20:16 +0200

    Added relative path
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,7 @@
 ./gradlew clean
 rm -rf .gradle
 rm -rf build
-rm -rf */build
+rm -rf ./*/build
 rm -rf iosApp/iosApp.xcworkspace
 rm -rf iosApp/Pods
 rm -rf iosApp/iosApp.xcodeproj/project.xcworkspace
