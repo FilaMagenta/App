8862045216dbaeda3d43c1f9aa9975adfb1a146a
Arnau Mora <arnyminerz@proton.me>
2023-10-16 19:26:28 +0200

    AppCompatDelegate now receives lang updates
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- androidApp/src/main/java/com/arnyminerz/filamagenta/android/MainActivity.kt
+++ androidApp/src/main/java/com/arnyminerz/filamagenta/android/MainActivity.kt
@@ -6,14 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import com.arnyminerz.filamagenta.storage.SettingsKeys
 import com.arnyminerz.filamagenta.storage.settings
 import com.arnyminerz.filamagenta.ui.screen.MainScreen
 import com.arnyminerz.filamagenta.ui.state.MainViewModel
 import com.arnyminerz.filamagenta.ui.theme.AppTheme
 import com.arnyminerz.filamagenta.utils.Language
+import com.russhwolf.settings.SettingsListener
 import com.russhwolf.settings.get
 import com.russhwolf.settings.set
+import io.github.aakira.napier.Napier
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -22,10 +25,25 @@ class MainActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<MainViewModel>()
 
+    private var languageChangeListener: SettingsListener? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         updateAppLocale()
+
+        languageChangeListener = settings.addStringListener(SettingsKeys.LANGUAGE, Language.System.langCode) { code ->
+            Napier.i("Updated app locale preference. Notifying compat...")
+            if (code == Language.System.langCode) {
+                AppCompatDelegate.setApplicationLocales(
+                    LocaleListCompat.getEmptyLocaleList()
+                )
+            } else {
+                AppCompatDelegate.setApplicationLocales(
+                    LocaleListCompat.forLanguageTags(code)
+                )
+            }
+        }
 
         setContent {
             AppTheme {
