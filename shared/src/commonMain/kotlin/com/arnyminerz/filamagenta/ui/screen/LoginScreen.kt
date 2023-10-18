43a8e57fa1c97e1a88d85d9a1aa70eba506d7825
Arnau Mora <arnyminerz@proton.me>
2023-10-18 20:21:35 +0200

    Disabled login button when logging in
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- shared/src/commonMain/kotlin/com/arnyminerz/filamagenta/ui/screen/LoginScreen.kt
+++ shared/src/commonMain/kotlin/com/arnyminerz/filamagenta/ui/screen/LoginScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.material.icons.rounded.ChevronLeft
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
@@ -165,7 +164,7 @@ fun LoginScreen(
 
             OutlinedButton(
                 onClick = ::login,
-                enabled = isDniValid && !isError,
+                enabled = isDniValid && !isError && !isLoggingIn,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp)
