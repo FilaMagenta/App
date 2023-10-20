243d52f5079f7df6fd60cad115e784879f0bb758
Arnau Mora <arnyminerz@proton.me>
2023-10-20 06:51:10 -0700

    Fixed function signature
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- shared/src/iosMain/kotlin/com/arnyminerz/filamagenta/sound/SoundPlayer.kt
+++ shared/src/iosMain/kotlin/com/arnyminerz/filamagenta/sound/SoundPlayer.kt
@@ -4,7 +4,7 @@ actual object SoundPlayer {
     /**
      * Plays the sound stored in a file in resources at [path]
      */
-    actual fun playFromResources(path: String) {
+    actual suspend fun playFromResources(path: String) {
         // TODO - currently not able to play sounds in iOS
     }
 }
