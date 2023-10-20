a864e3b0c09de523eadf33349840344b2ac9137f
Arnau Mora <arnyminerz@proton.me>
2023-10-20 10:48:15 +0200

    Added catch for invalid codes
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- shared/src/commonMain/kotlin/com/arnyminerz/filamagenta/image/QRCodeValidator.kt
+++ shared/src/commonMain/kotlin/com/arnyminerz/filamagenta/image/QRCodeValidator.kt
@@ -6,6 +6,7 @@ import com.arnyminerz.filamagenta.cache.data.qr.AccountQRCode
 import com.arnyminerz.filamagenta.cache.data.qr.ProductQRCode
 import com.arnyminerz.filamagenta.cache.database
 import com.arnyminerz.filamagenta.data.QrCodeScanResult
+import com.ionspin.kotlin.crypto.secretbox.SecretBoxCorruptedOrTamperedDataExceptionOrInvalidKey
 import io.github.aakira.napier.Napier
 import io.ktor.http.Url
 import kotlin.io.encoding.ExperimentalEncodingApi
@@ -61,82 +62,87 @@ object QRCodeValidator {
 
     @OptIn(ExperimentalEncodingApi::class, ExperimentalUnsignedTypes::class)
     suspend fun validateQRCode(source: String, result: MutableStateFlow<QrCodeScanResult?>, event: Event?) {
-        if (source.startsWith("app://filamagenta")) {
-            Napier.i("Got an NFC tag.")
+        try {
+            if (source.startsWith("app://filamagenta")) {
+                Napier.i("Got an NFC tag.")
 
-            Napier.v("Parsing the given source into an Url: $source")
-            val url = Url(source)
-            val query = url.encodedQuery
-                .split("&")
-                .associate {
-                    val pairs = it.split('=')
-                    pairs[0] to pairs[1]
+                Napier.v("Parsing the given source into an Url: $source")
+                val url = Url(source)
+                val query = url.encodedQuery
+                    .split("&")
+                    .associate {
+                        val pairs = it.split('=')
+                        pairs[0] to pairs[1]
+                    }
+
+                Napier.v("Checking if the url leads to an account...")
+                if (!url.pathSegments.contains("account")) {
+                    Napier.e("Got an unknown NFC tag.")
+                    return
                 }
 
-            Napier.v("Checking if the url leads to an account...")
-            if (!url.pathSegments.contains("account")) {
-                Napier.e("Got an unknown NFC tag.")
-                return
-            }
+                Napier.v("Checking if the url stored at the NFC tag has a code...")
+                if (!query.containsKey("code")) {
+                    Napier.e("NFC tag didn't have any code.")
+                    result.emit(QrCodeScanResult.Invalid)
+                    return
+                }
+                val code = query.getValue("code")
 
-            Napier.v("Checking if the url stored at the NFC tag has a code...")
-            if (!query.containsKey("code")) {
-                Napier.e("NFC tag didn't have any code.")
-                result.emit(QrCodeScanResult.Invalid)
-                return
-            }
-            val code = query.getValue("code")
+                Napier.v("Checking if there's an event currently open...")
+                if (event == null) {
+                    Napier.e("Currently not viewing any events. Ignoring scanned code...")
+                    result.emit(QrCodeScanResult.NotViewingEvent)
+                    return
+                }
 
-            Napier.v("Checking if there's an event currently open...")
-            if (event == null) {
-                Napier.e("Currently not viewing any events. Ignoring scanned code...")
-                result.emit(QrCodeScanResult.NotViewingEvent)
-                return
-            }
+                Napier.v("There's a loaded event. Decrypting the code...")
+                val qrCode = AccountQRCode.decrypt(code)
 
-            Napier.v("There's a loaded event. Decrypting the code...")
-            val qrCode = AccountQRCode.decrypt(code)
+                validateAccountQRCode(qrCode, event, result)
+            } else if (AccountQRCode.validate(source)) {
+                Napier.i("Got an account QR code.")
+                val qrCode = AccountQRCode.decrypt(source)
 
-            validateAccountQRCode(qrCode, event, result)
-        } else if (AccountQRCode.validate(source)) {
-            Napier.i("Got an account QR code.")
-            val qrCode = AccountQRCode.decrypt(source)
+                Napier.v("Checking if there's an event currently open...")
+                if (event == null) {
+                    Napier.e("Currently not viewing any events. Ignoring scanned code...")
+                    result.emit(QrCodeScanResult.NotViewingEvent)
+                    return
+                }
 
-            Napier.v("Checking if there's an event currently open...")
-            if (event == null) {
-                Napier.e("Currently not viewing any events. Ignoring scanned code...")
-                result.emit(QrCodeScanResult.NotViewingEvent)
-                return
-            }
+                validateAccountQRCode(qrCode, event, result)
+            } else if (ProductQRCode.validate(source)) {
+                Napier.i("Got a product QR code.")
+                val qrCode = ProductQRCode.decrypt(source)
 
-            validateAccountQRCode(qrCode, event, result)
-        } else if (ProductQRCode.validate(source)) {
-            Napier.i("Got a product QR code.")
-            val qrCode = ProductQRCode.decrypt(source)
+                Napier.i("Got valid QR, checking if stored...")
+                val ticket = database.adminTicketsQueries.getById(qrCode.orderId).executeAsOneOrNull()
+                if (ticket == null) {
+                    Napier.i("QR is not stored locally")
+                    result.emit(QrCodeScanResult.Invalid)
+                    return
+                }
 
-            Napier.i("Got valid QR, checking if stored...")
-            val ticket = database.adminTicketsQueries.getById(qrCode.orderId).executeAsOneOrNull()
-            if (ticket == null) {
-                Napier.i("QR is not stored locally")
-                result.emit(QrCodeScanResult.Invalid)
-                return
-            }
+                Napier.i("QR is stored, checking if reused...")
 
-            Napier.i("QR is stored, checking if reused...")
+                if (ticket.isValidated) {
+                    result.emit(QrCodeScanResult.AlreadyUsed)
+                } else {
+                    withContext(Dispatchers.Main) {
+                        Cache.updateIsValidated(qrCode.orderId, true)
+                    }
 
-            if (ticket.isValidated) {
-                result.emit(QrCodeScanResult.AlreadyUsed)
+                    result.emit(
+                        QrCodeScanResult.Success(qrCode.customerName, qrCode.orderNumber)
+                    )
+                }
             } else {
-                withContext(Dispatchers.Main) {
-                    Cache.updateIsValidated(qrCode.orderId, true)
-                }
-
-                result.emit(
-                    QrCodeScanResult.Success(qrCode.customerName, qrCode.orderNumber)
-                )
+                Napier.i("Got invalid QR")
+                result.emit(QrCodeScanResult.Invalid)
             }
-        } else {
-            Napier.i("Got invalid QR")
+        } catch (e: SecretBoxCorruptedOrTamperedDataExceptionOrInvalidKey) {
+            Napier.e("The scanned QR code or NFC tag is corrupted", throwable = e)
             result.emit(QrCodeScanResult.Invalid)
         }
     }
