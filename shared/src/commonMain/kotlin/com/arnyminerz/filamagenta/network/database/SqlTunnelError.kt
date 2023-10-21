dc59d9b0793c62efaf0f4871b4e7f1cb02da4e31
Arnau Mora <arnyminerz@proton.me>
2023-10-21 09:54:22 +0200

    Added more error keys
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- shared/src/commonMain/kotlin/com/arnyminerz/filamagenta/network/database/SqlTunnelError.kt
+++ shared/src/commonMain/kotlin/com/arnyminerz/filamagenta/network/database/SqlTunnelError.kt
@@ -5,5 +5,11 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class SqlTunnelError(
     val message: String? = null,
-    val code: String? = null
+    val code: String? = null,
+    val number: Int? = null,
+    val state: Int? = null,
+    val `class`: Int? = null,
+    val serverName: String? = null,
+    val procName: String? = null,
+    val lineNumber: Int? = null
 )
