1dae8f3542b8819a6e8c537489362123eb1c1545
Arnau Mora <arnyminerz@proton.me>
2023-11-04 15:06:37 +0100

    Updated to Kotlin 1.9.20
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- scripts/upload_sentry_ios.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# Install Sentry CLI: curl -sL https://sentry.io/get-cli/ | sh
-
-sentry-cli debug-files upload --auth-token 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 \
-  --include-sources \
-  --org fila-magenta \
-  --project apple-ios \
-  build/ios/Debug-iphonesimulator/
