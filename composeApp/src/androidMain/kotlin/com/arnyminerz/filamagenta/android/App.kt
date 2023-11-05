b743a25e3879289305f5187f35f7248e85b645a4
Arnau Mora <arnyminerz@proton.me>
2023-11-05 14:15:15 +0100

    Using Sentry-multiplatform
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/androidMain/kotlin/com/arnyminerz/filamagenta/android/App.kt
+++ composeApp/src/androidMain/kotlin/com/arnyminerz/filamagenta/android/App.kt
@@ -6,20 +6,15 @@ import com.arnyminerz.filamagenta.account.AccountsProvider
 import com.arnyminerz.filamagenta.account.accounts
 import com.arnyminerz.filamagenta.cache.DriverFactory
 import com.arnyminerz.filamagenta.cache.createDatabase
-import com.arnyminerz.filamagenta.device.Diagnostics
-import com.arnyminerz.filamagenta.device.MeasurementScope
-import com.arnyminerz.filamagenta.device.PerformanceMeasurer
 import com.arnyminerz.filamagenta.device.PlatformInformation
-import com.arnyminerz.filamagenta.diagnostics.SentryInformation
+import com.arnyminerz.filamagenta.diagnostics.SentryDiagnostics
 import com.arnyminerz.filamagenta.lifecycle.initialize
 import com.arnyminerz.filamagenta.sound.SoundPlayer
 import com.arnyminerz.filamagenta.storage.SettingsFactoryProvider
 import com.arnyminerz.filamagenta.storage.SettingsKeys
 import com.arnyminerz.filamagenta.storage.settings
 import com.arnyminerz.filamagenta.storage.settingsFactory
-import io.sentry.Sentry
-import io.sentry.android.core.SentryAndroid
-import io.sentry.protocol.User
+import io.sentry.kotlin.multiplatform.Sentry
 
 class App : Application() {
     override fun onCreate() {
@@ -29,72 +24,8 @@ class App : Application() {
 
         settingsFactory = SettingsFactoryProvider(this).factory
 
-        SentryAndroid.init(this) { options ->
-            options.dsn = SentryInformation.SentryDsn
-            options.isEnabled = settings.getBoolean(SettingsKeys.DATA_COLLECTION, true)
-            options.release = SentryInformation.ReleaseName
-            options.dist = SentryInformation.ReleaseName.substringBefore('-')
-            options.environment = if (SentryInformation.IsProduction) "prod" else "dev"
-            options.tracesSampleRate = 1.0
-        }
-        Diagnostics.updateUserInformation = { username, email ->
-            val user = User()
-            user.username = username
-            user.email = email
-            Sentry.setUser(user)
-        }
-        Diagnostics.deleteUserInformation = { Sentry.setUser(null) }
-        Diagnostics.performance = object : PerformanceMeasurer {
-            private inline fun <Result> perform(
-                name: String,
-                operation: String,
-                metadata: Map<String, Any>,
-                measurements: Map<String, Number>,
-                block: MeasurementScope.() -> Result
-            ): Result {
-                val transaction = Sentry.startTransaction(name, operation)
-                for ((key, value) in metadata) {
-                    transaction.setData(key, value)
-                }
-                for ((key, value) in measurements) {
-                    transaction.setMeasurement(key, value)
-                }
-                return try {
-                    block(
-                        object : MeasurementScope {
-                            override fun setMetadata(key: String, value: Any) {
-                                transaction.setData(key, value)
-                            }
-
-                            override fun setMeasurement(key: String, value: Number) {
-                                transaction.setMeasurement(key, value)
-                            }
-                        }
-                    )
-                } finally {
-                    transaction.finish()
-                }
-            }
-
-            override operator fun <Result> invoke(
-                name: String,
-                operation: String,
-                metadata: Map<String, Any>,
-                measurements: Map<String, Number>,
-                block: MeasurementScope.() -> Result
-            ): Result {
-                return perform(name, operation, metadata, measurements) { block() }
-            }
-
-            override suspend fun <Result> suspending(
-                name: String,
-                operation: String,
-                metadata: Map<String, Any>,
-                measurements: Map<String, Number>,
-                block: suspend MeasurementScope.() -> Result
-            ): Result {
-                return perform(name, operation, metadata, measurements) { block() }
-            }
+        SentryDiagnostics.initialize { configurator ->
+            Sentry.init(this) { configurator(it) }
         }
 
         PlatformInformation.hasCameraFeature = packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)
